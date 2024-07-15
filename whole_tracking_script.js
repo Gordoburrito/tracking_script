4b9f0b1b5dae277025a8812543003a1387a5e02f
Gordon Lewis <glewis@berkeley.edu>
2024-07-15 12:39:57 -0700

    update tracking script to use an initializing token

--- whole_tracking_script.js
+++ whole_tracking_script.js
@@ -1,192 +1,197 @@
-
-
 // https://cdn.jsdelivr.net/gh/roostergrin/tracking_script@1.0.0/whole_tracking_script.js
 
 // For Twortho
 // https://www.twortho.com/contact-us/request-appointment/
 
-// Function to collect initial tracking parameters
-function collectInitialPageVisitData() {
-  var url = document.referrer
-  var source = url
-    ? new URL(url).hostname.split('.').slice(-2, -1)[0]
-    : 'Direct or no-referrer'
-  var isFirstVisit = !localStorage.getItem('hasVisitedBefore')
-  var urlParams = new URLSearchParams(window.location.search)
-  var timestamp = new Date().toISOString()
+var CRM = {
+    init: function(config) {
+        this.token = config.token; // Store the authorization key
+        this.setupEventListeners(); // Setup event listeners after initialization
+        this.loadExternalScripts(); // Load external scripts like Fuse.js
+    },
 
-  if (isFirstVisit) localStorage.setItem('hasVisitedBefore', 'true')
+    setupEventListeners: function() {
+        window.addEventListener('load', this.handlePageVisit.bind(this));
+        window.addEventListener('beforeunload', this.transferSessionToLocalStorage.bind(this));
+        document.addEventListener('DOMContentLoaded', () => {
+            document.body.addEventListener('click', this.handleTelecomLinkClick.bind(this));
+            this.setupFormHandling.bind(this)();
+        });
+    },
 
-  return {
-    type: isFirstVisit ? 'initial_visit' : 'pageview',
-    source: urlParams.get('utm_source') || 'N/A',
-    medium: urlParams.get('utm_medium') || 'N/A',
-    campaign: urlParams.get('utm_campaign') || 'N/A',
-    term: urlParams.get('utm_term') || 'N/A',
-    content: urlParams.get('utm_content') || 'N/A',
-    referrerSource: source,
-    timestamp: timestamp,
-  }
-}
+    loadExternalScripts: function() {
+        const fuseScript = document.createElement('script');
+        fuseScript.src = 'https://cdn.jsdelivr.net/npm/fuse.js/dist/fuse.min.js';
+        fuseScript.onload = () => {
+            this.initializeFormHandling();
+        };
+        document.head.appendChild(fuseScript);
+    },
 
-// Function to handle a new event
-function handleEvent(type) {
-  try {
-    var existingSessionData = JSON.parse(sessionStorage.getItem('sessionTrackingData')) || { sessionId: new Date().toISOString(), trackingParams: collectInitialPageVisitData(), events: {} }
+    handlePageVisit: function() {
+        this.handleEvent('pageview');
+    },
 
-    var timestamp = new Date().toISOString()
-    existingSessionData.events[timestamp] = {
-      type: type,
-      date: timestamp,
-    }
+    handleFormSubmit: function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        this.handleEvent('form_submission');
+    },
 
-    sessionStorage.setItem('sessionTrackingData', JSON.stringify(existingSessionData))
-  } catch (error) {
-    console.error('Failed to handle event:', error)
-  }
-}
-
-// Function to transfer data from sessionStorage to localStorage
-function transferSessionToLocalStorage() {
-  try {
-    var sessionData = JSON.parse(sessionStorage.getItem('sessionTrackingData'))
-    if (sessionData) {
-      var existingLocalData = JSON.parse(localStorage.getItem('trackingHistory')) || []
-      existingLocalData[sessionData.sessionId] = sessionData
-      localStorage.setItem('trackingHistory', JSON.stringify(existingLocalData))
-    }
-    sessionStorage.removeItem('sessionTrackingData')
-  } catch (error) {
-    console.error('Failed to transfer session data:', error)
-  }
-}
-
-
-// Event handlers
-function handlePageVisit() {
-  handleEvent('pageview');
-}
-
-function handleFormSubmit(event) {
-  handleEvent('form_submission');
-}
-
-// Attach event listeners
-window.addEventListener('load', handlePageVisit);
-window.addEventListener('beforeunload', function () {
-  transferSessionToLocalStorage();
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Dynamically load Fuse.js
-    const fuseScript = document.createElement('script');
-    fuseScript.src = 'https://cdn.jsdelivr.net/npm/fuse.js/dist/fuse.min.js';
-    fuseScript.onload = function () {
-        // Fuse.js is now loaded and can be used
-        initializeFormHandling();
-    };
-    document.head.appendChild(fuseScript);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Get all forms on the page
-    const forms = document.querySelectorAll('form');
-
-    forms.forEach(function (form) {
-        form.addEventListener('submit', handleFormSubmit);
-        form.addEventListener('submit', async function (event) { 
-            event.preventDefault(); // Prevent the default form submission
-
-            // Create a new FormData object from the form
-            const formData = new FormData(form);
-
-            // Create an array from formData keys and values, mapping keys to cleaned form labels
-            const formDataArray = Array.from(formData.entries()).map(([key, value]) => {
-                const field = form.querySelector(`[name="${key}"]`);
-                const label = form.querySelector(`label[for="${field.id}"]`);
-                let labelText = label ? label.textContent.trim() : key; // Use label text or key as fallback
-                labelText = labelText.replace(/[^a-zA-Z\s]/g, ''); // Remove non-alphabetic characters
-                labelText = labelText.replace(/\s+/g, ' ').trim(); // Normalize white spaces and trim
-                return { key: labelText, value };
-            });
-            console.log("formDataArray", formDataArray)
-
-            // Define options for Fuse.js
-            const options = {
-                includeScore: true,
-                keys: ['key'] // Use 'key' because we are now using label text as key in formDataArray
+    handleEvent: function(type) {
+        try {
+            var existingSessionData = JSON.parse(sessionStorage.getItem('sessionTrackingData')) || { sessionId: new Date().toISOString(), trackingParams: this.collectInitialPageVisitData(), events: {} };
+            var timestamp = new Date().toISOString();
+            existingSessionData.events[timestamp] = {
+                type: type,
+                date: timestamp,
             };
+            sessionStorage.setItem('sessionTrackingData', JSON.stringify(existingSessionData));
+        } catch (error) {
+            console.error('Failed to handle event:', error);
+        }
+    },
 
-            // Create a Fuse instance
-            const fuse = new Fuse(formDataArray, options);
-
-            // Define a function to search and get data by fuzzy key name
-            function getFuzzyData(searchKey) {
-                const result = fuse.search(searchKey);
-                return result.length ? result[0].item.value : null;
+    transferSessionToLocalStorage: function() {
+        try {
+            var sessionData = JSON.parse(sessionStorage.getItem('sessionTrackingData'));
+            if (sessionData) {
+                var existingLocalData = JSON.parse(localStorage.getItem('trackingHistory')) || [];
+                existingLocalData[sessionData.sessionId] = sessionData;
+                localStorage.setItem('trackingHistory', JSON.stringify(existingLocalData));
             }
+            sessionStorage.removeItem('sessionTrackingData');
+        } catch (error) {
+            console.error('Failed to transfer session data:', error);
+        }
+    },
 
-            // Use getFuzzyData to fetch form data by fuzzy searching keys
-            const firstName = getFuzzyData('Name');
-            const lastName = getFuzzyData('Last');
-            const email = getFuzzyData('Email');
-            const phone = getFuzzyData('Phone');
+    handleTelecomLinkClick: async function(e) {
+        const link = e.target.closest('a');
+        if (!link) { return; }
+        const hrefType = link.href.startsWith('tel:') ? 'tel' : link.href.startsWith('sms:') ? 'sms' : null;
+        if (!hrefType) { return; }
+        const postUrl = 'https://api.threadcommunication.com/api/v1/tracking';
+        const sessionData = sessionStorage.getItem('sessionTrackingData');
+        const trackingData = sessionData ? JSON.parse(sessionData).trackingParams : {};
+        const { referrerSource, source: utmSource, campaign } = trackingData;
+        const params = {
+            phoneNumber: link.href.replace(/^tel:|^sms:/, ''),
+            time: new Date().toISOString(),
+            website: window.location.href,
+            referrer_source: referrerSource,
+            utm_source: utmSource,
+            campaign_name: campaign,
+            href_type: hrefType
+        };
+        try {
+            const response = await fetch(postUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams(params).toString()
+            });
+            const data = await response.json();
+            console.log('Success:', data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    },
 
-            // Log form data to console
-            console.log('Form Data:');
-            for (let [key, value] of formData.entries()) {
-              console.log(`${key}: ${value}`);
-            }
+    setupFormHandling: function() {
+        const forms = document.querySelectorAll('form');
+        forms.forEach((form) => {
+            form.addEventListener('submit', this.handleFormSubmit.bind(this));
+            form.addEventListener('submit', this.processAndSubmitForm.bind(this));
+        });
+    },
 
-            // Retrieve data from local storage and log it to console
-            console.log('Local Storage Data:');
-            for (let i = 0; i < localStorage.length; i++) {
-                const key = localStorage.key(i);
-                const value = localStorage.getItem(key);
-                console.log(`${key}: ${value}`);
-            }
+    processAndSubmitForm: async function(event) {
+        event.preventDefault(); // Prevent the default form submission
 
-          try {
-            console.log("firstName", firstName)
-            console.log("lastName", lastName)
-            console.log("email", email)
-            console.log("phone", phone)
-          // TODO: Add the following back in
-          
-            const trackingData = JSON.parse(sessionStorage.getItem('sessionTrackingData'))?.trackingParams
-            const referrerSource = trackingData?.referrerSource
-            const utmSource = trackingData?.source
-            const campaign = trackingData?.campaign
-            const baseUrl = 'https://mammoth-deep-ape.ngrok-free.app'
+        const form = event.target;
+        const formData = new FormData(form);
 
-            console.log("trackingData", trackingData)
-            debugger
+        // Create an array from formData keys and values, mapping keys to cleaned form labels
+        const formDataArray = Array.from(formData.entries()).map(([key, value]) => {
+            const field = form.querySelector(`[name="${key}"]`);
+            const label = form.querySelector(`label[for="${field.id}"]`);
+            let labelText = label ? label.textContent.trim() : key; // Use label text or key as fallback
+            labelText = labelText.replace(/[^a-zA-Z\s]/g, ''); // Remove non-alphabetic characters
+            labelText = labelText.replace(/\s+/g, ' ').trim(); // Normalize white spaces and trim
+            return { key: labelText, value };
+        });
 
+        // Define options for Fuse.js
+        const options = {
+            includeScore: true,
+            keys: ['key'] // Use 'key' because we are now using label text as key in formDataArray
+        };
+
+        // Create a Fuse instance
+        const fuse = new Fuse(formDataArray, options);
+
+        // Define a function to search and get data by fuzzy key name
+        function getFuzzyData(searchKey) {
+            const result = fuse.search(searchKey);
+            return result.length ? result[0].item.value : null;
+        }
+
+        // Use getFuzzyData to fetch form data by fuzzy searching keys
+        const firstName = getFuzzyData('Name');
+        const lastName = getFuzzyData('Last');
+        const email = getFuzzyData('Email');
+        const phone = getFuzzyData('Phone');
+
+        // Retrieve tracking data from session storage
+        const trackingData = JSON.parse(sessionStorage.getItem('sessionTrackingData'))?.trackingParams;
+        const { referrerSource, source: utmSource, campaign } = trackingData;
+
+        const baseUrl = 'https://api.threadcommunication.com';
+        const token = this.token || 'default_key_if_not_provided'; // Use the token from the CRM object
+        console.log(firstName, lastName, email, phone, referrerSource, utmSource, campaign);
+        try {
             const response = await fetch(`${baseUrl}/api/v1/website_leads`, {
-              method: 'POST',
-              headers: {
-                  'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmlnaW4iOiJodHRwczovL2NybS10aHJlYWQucm9vc3RlcmdyaW50ZW1wbGF0ZXMuY29tLyIsInByYWN0aWNlX2lkIjoiMzU1In0.aUzwgQdETpNeY42CoQNYNSTiVJ23zKIL2I1e6bDRNqI',
-                  'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                  first_name: firstName,
-                  last_name: lastName,
-                  email: email,
-                  phone: phone,
-                  referrer_source: referrerSource,
-                  utm_source: utmSource,
-                  campaign_name: campaign
-              })
+                method: 'POST',
+                headers: {
+                    'Authorization': token, // Use the passed authorization key
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    first_name: firstName,
+                    last_name: lastName,
+                    email: email,
+                    phone: phone,
+                    referrer_source: referrerSource,
+                    utm_source: utmSource,
+                    campaign_name: campaign
+                })
             });
 
             const responseData = await response.json();
             if (responseData.message) {
                 console.warn(responseData.message);
             }
-          } catch (error) {
+            console.log("Success:", responseData);
+        } catch (error) {
             console.error('New Lead not created in RG CRM.', error);
-          }
-            // Pause execution to keep the console log on the screen
-        });
-    });
-});
+        }
+    },
+
+    collectInitialPageVisitData: function() {
+        var url = document.referrer;
+        var source = url ? new URL(url).hostname.split('.').slice(-2, -1)[0] : 'Direct or no-referrer';
+        var isFirstVisit = !localStorage.getItem('hasVisitedBefore');
+        var urlParams = new URLSearchParams(window.location.search);
+        var timestamp = new Date().toISOString();
+        if (isFirstVisit) localStorage.setItem('hasVisitedBefore', 'true');
+        return {
+            type: isFirstVisit ? 'initial_visit' : 'pageview',
+            source: urlParams.get('utm_source') || 'N/A',
+            medium: urlParams.get('utm_medium') || 'N/A',
+            campaign: urlParams.get('utm_campaign') || 'N/A',
+            term: urlParams.get('utm_term') || 'N/A',
+            content: urlParams.get('utm_content') || 'N/A',
+            referrerSource: source,
+            timestamp: timestamp,
+        };
+    }
+};
